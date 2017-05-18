7cc0a3e54d3b79feb8d10e7462d5974b183945ef
Yuheng Wang <yxw9636@gmail.com>
2017-05-18 09:10:14 -0700

    Update 144_Binary_Tree_Preorder_Traversal.py

--- python/144_Binary_Tree_Preorder_Traversal.py
+++ python/144_Binary_Tree_Preorder_Traversal.py
@@ -6,10 +6,16 @@ class TreeNode(object):
        
 class Solution(object):
     def inorder(self, root):
+         """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
         if not root: return []
         ans, stack = [], [root]
         while stack:
+            node = stack.pop()
             if node:
+                # add node.val before going to all children
                 ans.append(node.val)
                 stack.append(node.right)
                 stack.append(node.left)
