b63a34f44e9576be9ecc61894a6f4480e908624d
Yuheng Wang <yxw9636@gmail.com>
2017-05-23 14:48:14 -0700

    Create 66_Plus_One.py

---
+++ python/66_Plus_One.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
+        # 将一个数字的每个位上的数字分别存到一个一维向量中，最高位在最开头，我们需要给这个数字加一，
+        # 即在末尾数字加一，如果末尾数字是9，那么则会有进位问题，而如果前面位上的数字仍为9，则需要继续向前进位。
+        # 具体算法如下：首先判断最后一位是否为9，若不是，直接加一返回，若是，则该位赋0，再继续查前一位，同样的方法，
+        # 知道查完第一位。如果第一位原本为9，加一后会产生新的一位，那么最后要做的是，查运算完的第一位是否为0，
+        # 如果是，则在最前头加一个1
+        
+        
+        n = len(digits)
+        
+        for i in range(n-1, -1, -1):
+            if digits[i]==9: 
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        
+        if digits[0] == 0:
+            digits = [1] + digits
+        return digits
+        
+        
+        
+ 
