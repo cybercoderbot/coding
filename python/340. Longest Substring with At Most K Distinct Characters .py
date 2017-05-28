e94b6f55a19f8a89a671aa7368eb6d72214493b7
Yuheng Wang <yxw9636@gmail.com>
2017-05-27 22:31:38 -0700

    Create 340. Longest Substring with At Most K Distinct Characters .py

---
+++ python/340. Longest Substring with At Most K Distinct Characters .py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+
+        """
+        The general idea is to iterate over string s.
+        Always put the character c and its location i in the dictionary d.
+        1) If the sliding window contains less than or equal to k distinct characters, 
+        simply record the return value, and move on.
+        
+        2) Otherwise, we need to remove a character from the sliding window.
+           Here's how to find the character to be removed:
+           Because the values in d represents the rightmost location of each character in the sliding window, 
+           in order to find the longest substring T, we need to locate the smallest location, and remove it 
+           from the dictionary, and then record the return value.
+        """
+    
+        # Use dictionary d to keep track of (character, location) pair,
+        # where location is the rightmost location that the character appears at
+        d = {}
+        left, ans = 0, 0
+        for i, c in enumerate(s):
+            d[c] = i
+            if len(d) > k:
+                left = min(d.values())
+                # print(left) 1, 2
+                del d[s[left]]
+                left += 1
+            ans = max(i - left + 1, ans)
+        return ans
+        
+        
+        
+        
