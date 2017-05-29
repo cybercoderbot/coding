82a368c29a61fe3d1549951015c18dbbe8ef16f6
Yuheng Wang <yxw9636@gmail.com>
2017-05-29 10:01:18 -0700

    Create 566. Reshape the Matrix.py

---
+++ python/566. Reshape the Matrix.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        # try:
+        #     return np.reshape(nums, (r,c)).tolist()
+        # except:
+        #     return nums
+            
+        h, w = len(nums), len(nums[0])
+        if h * w != r * c: 
+            return nums
+            
+        ans = []
+        for x in range(r):
+            row = []
+            for y in range(c):
+                row.append(nums[(x * c + y) / w][(x * c + y) % w])
+            ans.append(row)
+        return ans
+        
+        
+        
