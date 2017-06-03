b8685c70166197a00c5a615aba73802b7c7cef7e
Yuheng Wang <yxw9636@gmail.com>
2017-06-02 23:29:58 -0700

    Create 56. Merge Intervals.py

---
+++ python/56. Merge Intervals.py
@@ -0,0 +1,28 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        
+        if len(intervals) == 0: return []
+        intervals = sorted(intervals, key = lambda x: x.start)
+        res = [intervals[0]]
+        for n in intervals[1:]:
+            if n.start <= res[-1].end: res[-1].end = max(n.end, res[-1].end)
+            else: res.append(n)
+        return res
+        
+        # out = []
+        # for i in sorted(intervals, key=lambda i: i.start):
+        #     if out and i.start <= out[-1].end:
+        #         out[-1].end = max(out[-1].end, i.end)
+        #     else:
+        #         out += i,
+        # return out
