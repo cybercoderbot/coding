1a23f8549366a243d8998033b7704ad600d0bdef
Yuheng Wang <yxw9636@gmail.com>
2017-06-12 17:49:50 -0700

    Create 448. Find All Numbers Disappeared in an Array.cpp

--- cpp/448. Find All Numbers Disappeared in an Array.cpp
+++ cpp/448. Find All Numbers Disappeared in an Array.cpp
@@ -11,9 +11,9 @@ Output: [5,6]*/
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        /*这道题让我们找出数组中所有消失的数，这类问题的一个重要条件就是1 ≤ a[i] ≤ n (n = size of array)，不然很难在O(1)空间和O(n)时间内完成。
-       一种解法的思路路是，对于每个数字nums[i]，如果其对应的nums[nums[i] - 1]是正数，我们就赋值为其相反数，如果已经是负数了，就不变了，
-       那么最后我们只要把留下的整数对应的位置加入结果res中即可*/
+        /*这道题让我们找出数组中所有消失的数，这类问题的一个重要条件就是1 ≤ a[i] ≤ n (size of array)，不然很难在O(1)空间和O(n)时间内完成。
+       一种解法的思路路是，对于每个数字n，如果其对应的nums[n-1]是正数，我们就赋值为其相反数，如果已经是负数了，就不变了，
+       那么最后我们只要把留下的整数对应的位置加入结果ans中即可*/
         if (nums.empty()) return {};
         vector<int> ans;
         for (int n : nums){
