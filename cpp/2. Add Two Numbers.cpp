a8118e9b806f94913618f41f0b03bd3b4ac9b412
Yuheng Wang <yxw9636@gmail.com>
2017-06-24 10:06:38 -0700

    Update 2. Add Two Numbers.cpp

--- cpp/2. Add Two Numbers.cpp
+++ cpp/2. Add Two Numbers.cpp
@@ -6,13 +6,12 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/* 建立一个新链表，然后把输入的两个链表从头往后，每两个相加，添加一个新节点到新链表后面，就是要处理下进位问题。
+   还有就是最高位的进位问题要最后特殊处理一下.*/
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        // 建立一个新链表，然后把输入的两个链表从头往后撸，每两个相加，
-        // 添加一个新节点到新链表后面，就是要处理下进位问题。
-        // 还有就是最高位的进位问题要最后特殊处理一下.
         
         ListNode* res = new ListNode(-1);
         ListNode* curr = res;
