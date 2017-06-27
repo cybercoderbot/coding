e5d3d1e06153d0c1610682acfdb93a089b773de9
Yuheng Wang <yxw9636@gmail.com>
2017-06-26 23:58:39 -0700

    Update 337. House Robber III.cpp

--- cpp/337. House Robber III.cpp
+++ cpp/337. House Robber III.cpp
@@ -1,3 +1,24 @@
+/*The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root."
+Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this 
+place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+
+Example 2:
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.*/
+
 /*把已经算过的节点用哈希表保存起来，以后递归调用的时候，现在哈希表里找，如果存在直接返回，如果不存在，等计算出来后，
 保存到哈希表中再返回，这样方便以后再调用*/
 
