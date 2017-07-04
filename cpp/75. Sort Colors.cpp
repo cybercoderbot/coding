bcd4a8f2794a73a9b893303db23b2d0f63bb8395
Yuheng Wang <yxw9636@gmail.com>
2017-07-04 00:04:36 -0700

    Create 75. Sort Colors.cpp

---
+++ cpp/75. Sort Colors.cpp
@@ -0,0 +1,15 @@
+// Count sort
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+        int count[3] = {0}, idx = 0;
+        for (int i = 0; i < n; ++i) ++count[A[i]];
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < count[i]; ++j) {
+                A[idx++] = i;
+            }
+        }
+    }
+};
+
+
