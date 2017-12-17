3e063398b8ae965ef1aa67240671e7f61e0ed91d
Yuheng Wang <yxw9636@gmail.com>
2017-12-17 11:10:01 -0800

    Update 198. House Robber.cpp

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -1,9 +1,9 @@
-/*You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only 
-constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically 
-contact the police if two adjacent houses were broken into on the same night.
+/*You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and 
+it will automatically contact the police if two adjacent houses were broken into on the same night.
 
-Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can 
-rob tonight without alerting the police.*/
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of 
+money you can rob tonight without alerting the police.*/
 
 /*对于这类求极值的问题首先考虑动态规划Dynamic Programming来解，递推公式dp[i] = max(dp[i-2] + num[i], dp[i-1])*/
 
