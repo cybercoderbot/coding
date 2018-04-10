a653781c604342bdc99caf4a4c5a17c29ca6f550
Yuheng Wang <yxw9636@gmail.com>
2018-04-09 21:49:11 -0700

    update

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //O(n)的算法：遍历数组，建立map数据，遍历的同时开始查找，找到则记录index
-    
         unordered_map<int, int> m;
         for(int i=0; i<nums.size(); i++){
-            if(m.count(target-nums[i])){
-                return {i, m[target-nums[i]]};
+            int d = target-nums[i];
+            if(m.count(d)){
+                return {i, m[d]};
+            }else{
+                m[nums[i]] = i;
             }
-            m[nums[i]] = i;
         }
         return {};
         
