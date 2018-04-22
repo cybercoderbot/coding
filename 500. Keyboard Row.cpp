f43674b4a88a3597a24ea636fe25b61cbe229834
Yuheng Wang <yxw9636@gmail.com>
2018-04-22 08:58:07 -0700

    update

--- 500. Keyboard Row.cpp
+++ 500. Keyboard Row.cpp
@@ -9,13 +9,13 @@
 // You may use one character in the keyboard more than once.
 // You may assume the input string will only contain letters of alphabet.
 
+// 这道题给了我们一些单词，问哪些单词可以由键盘上的一行中的键符打出来。
+// 首先我们把键盘的三行字符分别保存到三个set中，然后遍历每个单词中的每个字符，分别看当前字符是否在三个集合中，
+// 如果在，对应的标识变量变为1，我们统计三个标识变量之和就知道有几个集合参与其中了
+
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
-        
-        // 这道题给了我们一些单词，问哪些单词可以由键盘上的一行中的键符打出来。
-        // 首先我们把键盘的三行字符分别保存到三个set中，然后遍历每个单词中的每个字符，分别看当前字符是否在三个集合中，
-        // 如果在，对应的标识变量变为1，我们统计三个标识变量之和就知道有几个集合参与其中了
         
         vector<string> ans;
         unordered_set<char> row1 = {'q','w','e','r','t','y','u','i','o','p'};
