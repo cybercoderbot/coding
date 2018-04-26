83adcd866263b0448cf4d536dbb632e9dbbd1e4f
Yuheng Wang <yxw9636@gmail.com>
2018-04-26 15:46:07 -0700

    update

--- 697.Degree of an Array.cpp
+++ 697.Degree of an Array.cpp
@@ -1,10 +1,10 @@
 /*697.Degree of an Array
 
-Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum 
-frequency of any one of its elements.
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the 
+maximum frequency of any one of its elements.
 
-Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same 
-degree as nums.
+Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the 
+same degree as nums.
 
 Example 1:
 
@@ -36,8 +36,8 @@ nums[i] will be an integer between 0 and 49,999. */
 再用一个哈希表，建立每个数字和其首尾出现的位置。我们用变量degree来表示数组的度。好，现在我们遍历原数组，累加当前数字
 出现的次数，当某个数字是第一次出现，那么我们用当前位置的来更新该数字出现的首尾位置，否则只更新尾位置。每遍历一个数，
 我们都更新一下degree。当遍历完成后，我们已经有了数组的度，还有每个数字首尾出现的位置，下面就来找出现次数为degree的数组，
-然后计算其首尾位置差加1就是candidate数组的长度，由于出现次数为degree的数字不一定只有一个，我们遍历所有的，找出其中最小的即可
-*/
+然后计算其首尾位置差加1就是candidate数组的长度，由于出现次数为degree的数字不一定只有一个，我们遍历所有的，找出其中最小
+的即可*/
 
  
 
