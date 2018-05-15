dfa3893fb2f92f6d102c386db646abb478267bc8
Yuheng Wang <yxw9636@gmail.com>
2018-05-14 23:18:09 -0700

    update

--- 213. House Robber II.cpp
+++ 213. House Robber II.cpp
@@ -1,14 +1,15 @@
-/*Note: This is an extension of House Robber.
-After robbing those houses on that street, the thief has found himself a new place for his thievery so that he will not get too much
-attention. This time, all houses at this place are arranged in a circle. That means the first house is the neighbor of the last one.
-Meanwhile, the security system for these houses remain the same as for those in the previous street.
+/* Note: This is an extension of House Robber.
+After robbing those houses on that street, the thief has found himself a new place for his thievery so that 
+he will not get too much attention. This time, all houses at this place are arranged in a circle. 
+That means the first house is the neighbor of the last one. Meanwhile, the security system for these houses 
+remain the same as for those in the previous street.
 
-Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can 
-rob tonight without alerting the police.*/
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum 
+amount of money you can rob tonight without alerting the police.*/
 
-/*这道题是之前那道House Robber 打家劫舍的拓展，现在房子排成了一个圆圈，则如果抢了第一家，就不能抢最后一家，因为首尾相连了，所以第一家和最后一家
-只能抢其中的一家，或者都不抢，那我们这里变通一下，如果我们把第一家和最后一家分别去掉，各算一遍能抢的最大值，然后比较两个值取其中较大的一个即为所求。
-那我们只需参考之前的House Robber 打家劫舍中的解题方法，然后调用两边取较大值*/
+/*这道题是之前那道House Robber 打家劫舍的拓展，现在房子排成了一个圆圈，则如果抢了第一家，就不能抢最后一家，因为首尾相连了，
+所以第一家和最后一家只能抢其中的一家，或者都不抢，那我们这里变通一下，如果我们把第一家和最后一家分别去掉，各算一遍能抢的最大值，
+然后比较两个值取其中较大的一个即为所求。那我们只需参考之前的House Robber 打家劫舍中的解题方法，然后调用两边取较大值*/
 
 class Solution {
 public:
