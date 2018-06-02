8293d2531daf3b30ab79cf40be7b51d1e68f79dc
Yuheng Wang <yxw9636@gmail.com>
2018-06-02 12:02:31 -0700

    update

--- 36. Valid Sudoku.cpp
+++ 36. Valid Sudoku.cpp
@@ -1,4 +1,10 @@
 /*36. Valid Sudoku
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
 
 Three flags are used to check whether a number appear.
 used1: check each row
@@ -17,7 +23,7 @@ public:
             for(int j = 0; j < board[i].size(); j++){
                 if(board[i][j] != '.'){
                     int num = board[i][j] - '0' - 1;
-                    int k = i / 3 * 3 + j / 3;
+                    int k = 3 * (i / 3) + j / 3;
                     if(used1[i][num] || used2[j][num] || used3[k][num]){
                         return false;
                     }
