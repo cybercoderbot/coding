79c4653e9cf2666327f014b69f184a64f47a2331
Yuheng Wang <yxw9636@gmail.com>
2018-06-12 00:42:50 -0700

    update

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -8,8 +8,9 @@ Note:
 You may assume the string contains only lowercase alphabets.*/
  
 /*这题核心点就在于使用哈希表映射. 用一个数组来代替哈希表，使用类似方法的题目有Minimum Window Substring 最小窗口子串，
-Isomorphic Strings 同构字符串，Longest Substring Without Repeating Characters 最长无重复子串 和 1.1 Unique Characters of a String 
-字符串中不同的字符。然后把s中所有的字符出现个数统计起来，存入一个unordered_map中。然后我们再来统计t字符串，如果发现不匹配则返回false*/
+Isomorphic Strings 同构字符串，Longest Substring Without Repeating Characters 最长无重复子串 和 1.1 Unique 
+Characters of a String 字符串中不同的字符。然后把s中所有的字符出现个数统计起来，存入一个unordered_map中。
+然后我们再来统计t字符串，如果发现不匹配则返回false*/
 
 class Solution {
 public:
@@ -22,6 +23,16 @@ public:
             if (m[c]) return false;
         return true;
         
+    }
+};
+
+/* 简单粗暴，直接sort两个string，sort后相同则为anagram，否则不是。复杂度o(nlogn) */
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;  
     }
 };
 
